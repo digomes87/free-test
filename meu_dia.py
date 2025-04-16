f7502d71c7e1d462d5ebeb3c30e2e2ca11b1c235
Diego Go <diego.gomes87@gmail.com>
2025-04-16 18:54:41 +0200

    List Callable

---
+++ meu_dia.py
@@ -0,0 +1,49 @@
+from typing import List, Callable
+
+
+def acordar():
+    print("Acordei vamos começar o dia")
+
+def escovar_dentes():
+    print("Esconvando os dentes")
+
+def tomar_cafe():
+    print("Tomando cafe")
+
+def ir_pra_escola():
+    print("Indo pra escola")
+
+def estudar():
+    print("Estudando")
+
+def brincar():
+    print("Brincando")
+
+def jantar():
+    print("Brincando")
+
+def dormir():
+    print("Dormindo")
+
+
+def dia_de_uma_crianca(tarefas: List[Callable]):
+    print("Começando o dia 👌\n")
+    for tarefa in tarefas:
+        tarefa()
+
+    print("Fim do Dia")
+
+meu_dia = [
+    acordar,
+    escovar_dentes,
+    tomar_cafe,
+    ir_pra_escola,
+    estudar,
+    brincar,
+    jantar,
+    dormir
+]
+
+
+if __name__ == "__main__":
+    dia_de_uma_crianca(meu_dia)
